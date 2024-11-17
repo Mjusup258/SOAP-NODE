cf06f1015c4a87680c01846ef7d84bcd08319bce
Maja Jusup <mjusup258@gmail.com>
2024-11-17 22:49:38 +0100

    add soap api credentials and authentication operations

---
+++ credentials/soapAPIRequest.credentials.ts
@@ -0,0 +1,35 @@
+import {
+	IAuthenticateGeneric,
+	ICredentialType,
+	INodeProperties,
+} from 'n8n-workflow';
+
+export class SoapApi implements ICredentialType {
+  name = 'soapApi';
+  displayName = 'SOAP API';
+  properties: INodeProperties[] = [
+    {
+      displayName: 'Username',
+      name: 'username',
+      type: 'string',
+      default: '',
+    },
+    {
+      displayName: 'Password',
+      name: 'password',
+      type: 'string',
+      typeOptions: {password: true},
+      default: '',
+    }
+  ];
+
+  authenticate: IAuthenticateGeneric = {
+    type: 'generic',
+    properties: {
+      headers: {
+        username: '={{$credentials.username}}',
+        password: '={{$credentials.password}}',
+      },
+    },
+  };
+}
